f680459e3594311b3afc9e23d3d774a0ec19332c
Nancy Goswami <018tmpind@Nancys-MacBook-Pro.local>
2023-05-18 19:55:06 +0530

    2nd way of authentication commented

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,32 +1,31 @@
 class ApplicationController < ActionController::API
-    # def encode_token(payload)
-    #     JWT.encode(payload,'secret')
-    # end
     SECRET = "yoursecretword"
 
-    def authentication
-      decode_data = decode_user_data(request.headers["token"])
-      user_data = decode_data[0]["user_id"] unless !decode_data
-      user = User.find(user_data&.id)
-  
-      if user
-        return true
-      else
-        render json: { message: "invalid credentials" }
-      end
-    end
-  
     def encode_user_data(payload)
-      JWT.encode payload, SECRET, "HS256"
+        JWT.encode payload, SECRET, "HS256"
     end
-  
+    
     def decode_user_data(token)
-      begin
-        JWT.decode token, SECRET, true, { algorithm: "HS256" }
-      rescue => e
-        puts e
-      end
+        begin
+            JWT.decode token, SECRET, true, { algorithm: "HS256" }
+        rescue => e
+            puts e
+        end
     end
 end
+    
+
+    # 2nd way to authenticate: token decoding 
+    # def authentication
+    #   decode_data = decode_user_data(request.headers["token"])
+    #   user_data = decode_data[0]["user_id"] unless !decode_data
+    #   user = User.find(user_data&.id)
+  
+    #   if user
+    #     return true
+    #   else
+    #     render json: { message: "invalid credentials" }
+    #   end
+    # end
 
 # code that can run in every controller
